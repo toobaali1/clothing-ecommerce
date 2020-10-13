93dd27af4eec82460ab7a99975a10acfbfddef09
Tooba Ali <toobaalined@gmail.com>
2020-10-13 08:45:59 +0500

    Modify App.js to access authenticated session

--- src/App.js
+++ src/App.js
@@ -4,10 +4,35 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shoppage/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component";
 import Header from "./components/header/header.component"
-import {Route, Switch, Link} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
+import {auth} from "./firebase/firebase.utils";
+// import { render } from '@testing-library/react';
 
 
-function App() {
+class App extends React.Component {
+constructor(){
+  super();
+
+  this.state = {
+    currentUser: null
+  }
+}
+
+unsubscribeFromAuth = null;
+
+componentDidMount(){
+  this.unsubscribeFromAuth = auth.onAuthStateChanged(user =>{
+    this.setState({currentUser: user});
+    console.log(user);
+  });
+
+}
+
+componentWillUnmount(){
+  this.unsubscribeFromAuth();
+}
+
+  render(){
   return (
     <div>
     <Header />
@@ -18,6 +43,7 @@ function App() {
       </Switch>
     </div>
   );
+  }
 }
 
 export default App;
