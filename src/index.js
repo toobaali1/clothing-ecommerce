5ef214533353f807a21acbd77bde25daa40ae4e4
Tooba Ali <toobaalined@gmail.com>
2020-10-15 16:15:19 +0500

    Install redux,react-redux and redux-logger; Initial configuration of REDUX in index.js; Create a userReducer and export to root-reducer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import {BrowserRouter} from "react-router-dom";
-import './index.css';
+import {Provider} from "react-redux";
+
 import App from './App';
+import './index.css';
 
 
 ReactDOM.render(
-<BrowserRouter>
-<App />
-</BrowserRouter>
+    <Provider>
+        <BrowserRouter>
+        <App />
+        </BrowserRouter>
+    </Provider>
 ,document.getElementById('root'));
 
 
